525fcddd6ba36368af0017a3e8376118b129c3f1
Robert Weingart <raw20@imperial.ac.uk>
2024-06-03 23:29:02 +0100

    Change solver to fail if constraint can't be solved right away, for better errors

--- src/TestPlugin/Solver.hs
+++ src/TestPlugin/Solver.hs
@@ -4,11 +4,13 @@ import GHC.Plugins hiding (TcPlugin)
 import GHC.Tc.Plugin
 
 import GHC.Tc.Types.Evidence
-import GHC.Tc.Utils.Monad (TcPlugin (..), TcPluginSolveResult (..), mapAndUnzipM)
-import GHC.Tc.Types.Constraint (Ct, ctPred, mkNonCanonical, ctLoc)
+import GHC.Tc.Utils.Monad (TcPlugin (..), TcPluginSolveResult (..), mapMaybeM)
+import GHC.Tc.Types.Constraint (Ct, ctPred, mkNonCanonical, ctLoc, mkSimpleWC, ctEvidence, isSolvedWC)
 import GHC.Core.Class (Class(..))
 import GHC.Core.Predicate (getClassPredTys_maybe)
 import TestPlugin.Placeholder (mkPlaceholder)
+import GHC.Tc.Solver (solveWanteds)
+import GHC.Tc.Solver.Monad (runTcS)
 
 totalTcPlugin :: [CommandLineOption] -> Maybe TcPlugin
 totalTcPlugin _ = Just $ 
@@ -18,17 +20,12 @@ totalTcPlugin _ = Just $
            , tcPluginStop = \_ -> return ()
            }
 
-canBeTotalCt :: Ct -> Bool
-canBeTotalCt ct = case getClassPredTys_maybe $ ctPred ct of
-  Just (cls, _) -> all (isAlgType . varType) (classTyVars cls)
-  Nothing -> False
-
 totalClassName :: TcPluginM Name
 totalClassName = do
   Found _ md <- findImportedModule (mkModuleName "TestPlugin") NoPkgQual
   lookupOrig md (mkTcOcc "TotalClass")
 
-wantedCtToTotal :: Ct -> TcPluginM ((EvTerm, Ct), Ct)
+wantedCtToTotal :: Ct -> TcPluginM (Maybe (EvTerm, Ct))
 wantedCtToTotal ct = do
   targetTyConTy <- case getClassPredTys_maybe $ ctPred ct of
     Just (cls, _) -> return $ mkTyConTy $ classTyCon cls
@@ -36,17 +33,17 @@ wantedCtToTotal ct = do
   totalClass <- totalClassName >>= tcLookupClass
   let predType = mkTyConApp (classTyCon totalClass) [typeKind targetTyConTy, targetTyConTy] 
   newCt <- mkNonCanonical <$> newWanted (ctLoc ct) predType
-  return ((mkPlaceholder predType, ct), newCt)
+  (wc, _) <- unsafeTcPluginTcM $ runTcS $ solveWanteds (mkSimpleWC [ctEvidence newCt])
+  if isSolvedWC wc then return $ Just (mkPlaceholder predType, ct) else return Nothing
 
 solve :: () -> EvBindsVar -> [Ct] -> [Ct] -> TcPluginM TcPluginSolveResult
 solve () _ _ [] = do
   return $ TcPluginSolveResult [] [] []
 solve () _ _ wanteds = do
-  (solvedCts, newCts) <- mapAndUnzipM wantedCtToTotal $ filter canBeTotalCt wanteds
-  output "Adding TotalClass constraints:" newCts
+  solvedCts <- mapMaybeM wantedCtToTotal wanteds
   return $ TcPluginSolveResult 
     { tcPluginSolvedCts = solvedCts
-    , tcPluginNewCts = newCts
+    , tcPluginNewCts = []
     , tcPluginInsolubleCts = []
     }
 
